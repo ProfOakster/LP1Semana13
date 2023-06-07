961a2b4c0a67697a8b9401ecaebae21f0d91f732
TomasCarvalho <2606tom@gmail.com>
2023-06-07 21:54:51 +0100

    Add method GetMinMax2

--- MultipleStuff/Program.cs
+++ MultipleStuff/Program.cs
@@ -9,7 +9,10 @@ namespace MultipleStuff
             SuperList sl = new SuperList(){ 4f, -4f, 256f, 32256f};
 
             sl.GetMinMax1(out double min1, out double max1);
-            Console.WriteLine($"GetMinMax1: min-{min1}; max-{max1}");
+            Console.WriteLine($"GetMinMax1: min-|{min1}|; max-|{max1}|");
+
+            MinMax minmax2= sl.GetMinMax2();
+            Console.WriteLine($"GetMinMax1: min-|{minmax2.Min}|; max-|{minmax2.Max}|");
         }
     }
 }
--- MultipleStuff/SuperList.cs
+++ MultipleStuff/SuperList.cs
@@ -12,14 +12,39 @@ namespace MultipleStuff
         {
             min = double.MaxValue;
             max = double.MinValue;
-           
+
             foreach (double num in this)
             {
                 if (num < min) min = num;
-                if (num > max) max = num; 
+                if (num > max) max = num;
             }
         }
+
+        public MinMax GetMinMax2()
+        {
+            double min = double.MaxValue;
+            double max = double.MinValue;
+
+            foreach (double num in this)
+            {
+                if (num < min) min = num;
+                if (num > max) max = num;
+            }
+            return new MinMax(min, max);
+        }
     }
+    public struct MinMax
+    {
+        public double Min { get; }
+        public double Max { get; }
+
+        public MinMax(double min, double max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+
 
 
 
