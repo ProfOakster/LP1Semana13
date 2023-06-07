feaabf6a24ac5ef0c45573230c96e2921a69f3fd
TomasCarvalho <2606tom@gmail.com>
2023-06-07 18:28:43 +0100

    Create method Palindrome

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -8,5 +8,37 @@ namespace Palindrome
         {
             Console.WriteLine("Hello, World!");
         }
-    }
+
+        static bool Palindrome(string testing)
+        {
+            bool CheckedPalindrome(string testing)
+            {
+                if (testing.EndsWith(testing[0]))
+                {
+                    testing.Remove(0, 1);
+                    testing.Remove(testing.Length, 1);
+                    
+                    return Program.Palindrome(testing);
+                }
+                else
+                    return false;
+            }
+
+
+            if (testing is null)
+            {
+                throw new ArgumentNullException();
+            }
+            else if (testing.Length<2)
+            {
+                return true;
+            }
+            else
+                return CheckedPalindrome(testing);
+        }
+
+
+
+    }   
+
 }
